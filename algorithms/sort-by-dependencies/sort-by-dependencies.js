ab4eab8d9bd320c6d0f00e3cd8cd9e1941a7c447
k9wolf8634 <k9wolf@pm.me>
2022-07-21 15:01:51 +0800

    Slight remodel of code

--- algorithms/sort-by-dependencies/sort-by-dependencies.js
+++ algorithms/sort-by-dependencies/sort-by-dependencies.js
@@ -4,13 +4,15 @@
 // A.before = [ "B" ] means that A must be before B in the array
 
 
-function getObj(name, list) {
-	return list.find(obj => obj.name === name);
+let loop = true;
+
+function getObj(name, arr) {
+	return arr.find(obj => obj.name === name);
 }
 
-let loop = true;
+// Manages dependencies by expanding and re-ordering the array
 // If we have A.before = [ "B" ], B.before = [ "C" ], A.before will be expanded into [ "B", "C" ].
-function expandDependencies(obj, depen, list) {
+function manageDependencies(obj, depen, arr) {
 	if(!depen || !loop) return;
 	if(depen === []) return;
 
@@ -19,52 +21,48 @@ function expandDependencies(obj, depen, list) {
 			console.log(`${name} was linked back to itself as a dependency. Quitting.`); // TODO: find path
 			loop = false;
 		} else {
-			_nextDepen = getObj(name, list).before;
+			let beforeObj = getObj(name, arr);
+			let _nextDepen = beforeObj.before;
 			if(!_nextDepen) continue;
 
 			nextDepen = _nextDepen.filter(n => !obj.before.includes(n));
 			obj.before = obj.before.concat(nextDepen);
-			expandDependencies(obj, nextDepen, list); // Iterate again with new dependencies
+			manageDependencies(obj, nextDepen, arr); // Iterate again with new dependencies
+
+			if(arr.indexOf(obj) > arr.indexOf(beforeObj)) { // After the obj which it needs to come before in the array
+				arr.splice(arr.indexOf(obj), 1);            // Remove obj
+				arr.splice(arr.indexOf(beforeObj), 0, obj); // Insert obj right before beforeObj
+			}
 		}
 	}
 }
 
 // Fixing befores then fixing afters isn't correct logic
-function afterIntoBefore(obj, list) {
+function afterIntoBefore(obj, arr) {
 	if(!obj.name || !obj.after) return;
 	for(var name of obj.after) {
-		let beforeList = getObj(name, list).before;
+		let beforeList = getObj(name, arr).before;
 		if(beforeList.includes(obj.name)) return;
 
-		getObj(name, list).before = beforeList.concat(obj.name);
+		getObj(name, arr).before = beforeList.concat(obj.name);
 	}
 	delete obj.after;
 }
 
 
 // Main function
-function sortByDependencies(list) {
-	for(var obj of list) {
+function sortByDependencies(arr) {
+	for(var obj of arr) {
 		if(!obj.before) obj.before = [];
 	}
-	list.forEach(obj => afterIntoBefore(obj, list));
-	list.forEach(obj => expandDependencies(obj, obj.before, list));
+
+	// Main body
+	let res = [...arr];
+	res.forEach(obj => afterIntoBefore(obj, res));
+	res.forEach(obj => manageDependencies(obj, obj.before, res));
+
 	if(!loop) return;
-
-	let res = [...list];
-
-	// Loop through every object
-	for(var obj of res) {
-		// Loop through each dependency of the object
-		for(var beforeName of obj.before) {
-			let beforeObj = getObj(beforeName, res);
-			if(res.indexOf(obj) > res.indexOf(beforeObj)) { // After the obj which it needs to come before in the array
-				res.splice(res.indexOf(obj), 1);            // Remove obj
-				res.splice(res.indexOf(beforeObj), 0, obj); // Insert obj right before beforeObj
-			}
-		}
-	}
 	return res;
 }
 
-module.exports = { sortByDependencies, expandDependencies };
+module.exports = { sortByDependencies, manageDependencies };
